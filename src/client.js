9d6df4628557aebc5131f162ad14c107b2c27754
Alois <alois@troposhq.com>
2018-05-04 22:55:04 -0400

    added create service

--- src/client.js
+++ src/client.js
@@ -1,39 +1,62 @@
 const axios = require('axios');
 
-class Client {
-  constructor(opts) {
-    this.adminAPIHost = opts.adminAPIHost || 'http://localhost';
-    this.adminAPIPort = opts.adminAPIPort || '8001';
-    this.kongAdminAPIURL = `${this.adminAPIHost}:${this.adminAPIPort}`
-  }
+function Client(opts) {
+  if (!(this instanceof Client)) { return new Client(); }
 
-  async createConsumer(username, customId) {
-    const consumerResult = await axios({
-      method: 'POST',
-      url: `${this.kongAdminAPIURL}/consumers`,
-      data: {
-        custom_id: `${customId}`,
-        username: `${username}`,
-      },
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then(x => x.data);
+  this.adminAPIHost = opts.adminAPIHost || 'http://localhost';
+  this.adminAPIPort = opts.adminAPIPort || '8001';
+  this.kongAdminAPIURL = `${this.adminAPIHost}:${this.adminAPIPort}`;
+}
 
-    return consumerResult;
-  }
+Client.prototype.createConsumer = async function ({ username, customId }) {
+  const consumerResult = await axios({
+    method: 'POST',
+    url: `${this.kongAdminAPIURL}/consumers`,
+    data: {
+      custom_id: `${customId}`,
+      username: `${username}`,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(x => x.data);
 
-  async createJWTCredential(consumerIDOrUsername) {
-    const credential = await axios({
-      method: 'POST',
-      url: `${this.kongAdminAPIURL}/consumers/${consumerIDOrUsername}/jwt`,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    }).then(x => x.data);
+  return consumerResult;
+}
 
-    return credential;
-  }
+Client.prototype.createJWTCredential = async function (consumerIDOrUsername) {
+  const credential = await axios({
+    method: 'POST',
+    url: `${this.kongAdminAPIURL}/consumers/${consumerIDOrUsername}/jwt`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  }).then(x => x.data);
+
+  return credential;
+}
+
+Client.prototype.createService = async function ({
+  name,
+  protocol,
+  host,
+  port,
+  path,
+  retries,
+  connect_timeout,
+  write_timeout,
+  read_timeout,
+  url,
+}) {
+  const service = await axios({
+    method: 'POST',
+    url: `${this.kongAdminAPIURL}/services`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(x => x.data);
+
+  return service;
 }
 
 module.exports = Client;
